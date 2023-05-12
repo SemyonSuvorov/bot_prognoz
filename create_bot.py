08f79386aff796eda250325a9b328286af5c6d20
SemyonSuvorov <semen.suvoroff@yandex.ru>
2023-05-12 14:34:10 +0300

    newcommit

--- create_bot.py
+++ create_bot.py
@@ -3,6 +3,7 @@ from aiogram.dispatcher import Dispatcher
 import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+storage = MemoryStorage()
 bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
 open_weather_token = os.getenv('OWTOKEN')
--- tg_bot.py
+++ tg_bot.py
@@ -1,12 +1,14 @@
 from aiogram.utils import executor
 from create_bot import dp
 from handlers import client, admin, other
-
+from art import tprint
 
 async def on_startup(_):
-    print("Bot launched successfully!")
+    tprint('''BOT LAUNCHED 
+    SUCCESSIFULLY''', font='bulbhead')
 
-
+admin.register_handlers_admin(dp)
 client.register_handlers_client(dp)
+
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
