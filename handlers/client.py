2112afc0e955284ad947e5916e951ec1ab2e4772
SemyonSuvorov <semen.suvoroff@yandex.ru>
2023-05-24 23:00:24 +0300

    lesgooo

--- handlers/client.py
+++ handlers/client.py
@@ -1,27 +1,36 @@
 from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.filters import Text
 from create_bot import bot, open_weather_token
-from keyboards.client_kb import send_loc_kb
+from keyboards import client_kb
 from aiogram.types import ReplyKeyboardRemove
 import requests
+
+class FSMClient1(StatesGroup):
+    ask_city = State()
+
 
 
 async def command_start(message: types.message):
     if message.text == '/start':
         try:
             await bot.send_message(message.from_user.id,
-                                   'Привет! Вы можете получить прогноз погоды, воспользовавшись меню ниже:',
-                                   reply_markup=send_loc_kb)
+                                   'Привет! Вы можете получить прогноз одежды либо прогноз погоды, воспользовавшись меню ниже:',
+                                   reply_markup=client_kb.welcome_kb)
 
         except:
             await message.reply('Общение с ботом через лс, напишите ему:\n@cloforecastbot')
 
+async def weather_choice(message: types.message):
+    await bot.send_message(message.from_user.id, 'Вы можете отправить название города, либо отправить свою геолокацию:', reply_markup=client_kb.send_loc_kb)
 
 async def get_city(message: types.message):
-    await message.answer('Введите название города:')
+    await bot.send_message(message.from_user.id, 'Введите название города:')
+    await FSMClient1.ask_city.set()
 
 
-async def city_info(message: types.message):
+async def city_info(message: types.message, state: FSMContext):
     code_to_smile = {
         "Clear": "Ясно \U0001F505",
         "Clouds": "Облачно \U000026C5",
@@ -48,8 +57,10 @@ async def city_info(message: types.message):
         feels_like = data["main"]["feels_like"]
         await message.reply(f"Погода в {city}:\nТемпература: {cur_weather} градусов\nОщущается как: {feels_like} "
                             f"градусов \nВлажность: {humidity} процентов\nСкорость ветра: {wind_speed} м/c\n{wd}")
+        await state.finish()
     except:
         await message.reply("\U00002620 Проверьте название города \U00002620")
+        await state.finish()
 
 
 async def handle_location(message: types.Message):
@@ -84,9 +95,13 @@ async def handle_location(message: types.Message):
     except:
         await message.reply("\U00002620 Некорректно отправлена геолокация \U00002620")
 
+async def catcher(message: types.Message):
+    await message.reply('Пожалуйста, поспользуйтесь командой на клавиатуре')
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
+    dp.register_message_handler(weather_choice, Text(equals='Прогноз погоды'))
     dp.register_message_handler(get_city, Text(equals='Отправить город', ignore_case=True))
+    dp.register_message_handler(city_info, state=FSMClient1.ask_city)
     dp.register_message_handler(handle_location, content_types=['location'])
-    dp.register_message_handler(city_info)
+    dp.register_message_handler(catcher)
--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,5 +1,10 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 send_loc_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-send_loc_kb.add(KeyboardButton(text='Отправить свою локацию🗺️', request_location=True),
-                KeyboardButton(text='Отправить город'))
+send_loc_kb.add(KeyboardButton(text='Отправить геолокацию🗺️', request_location=True),
+                KeyboardButton(text='Отправить город'),
+                KeyboardButton(text='Прогноз одежды'))
+
+welcome_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+welcome_kb.add(KeyboardButton(text='Прогноз одежды'),
+                KeyboardButton(text='Прогноз погоды'))
